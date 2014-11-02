a945bf61e7733c774d6b8108fbfcf3a895a68475
Ubuntu <ubuntu@ip-172-31-31-13.us-west-2.compute.internal>
2014-11-02 14:31:43 +0000

    ReadSync try no.1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
 
+var fs = require('fs');
+var file = fs.readFileSync("index.html", "utf8");
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(file)
 })
 
 app.listen(app.get('port'), function() {
