218805c8a8e35c42451b89b2728b7ed5efe05e9b
Ubuntu <ubuntu@ip-172-31-31-13.us-west-2.compute.internal>
2014-11-02 14:36:38 +0000

    Part 1 finished

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 
 var fs = require('fs');
-var file = fs.readFileSync("index.html", "utf8");
+var file = fs.readFileSync("index.html","utf8");
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
